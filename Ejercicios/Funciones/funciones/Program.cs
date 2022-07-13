c2dc7b9c06034c1985d0c1e4ff69fbbd1db8f69a
Paola Rivera <priveraa@unah.hn>
2022-07-13 17:37:48 -0600

    agregando ejercicio POO

--- Ejercicios/Funciones/funciones/Program.cs
+++ Ejercicios/Funciones/funciones/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno(1, "Juan", "Perez");
+            Alumno b = new Alumno(2, "Maria", "Martinez");
+
+            a.inactivarAlumno();
+
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
+            a.activarAlumno();
+
+            Console.WriteLine("");
+
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+        }
+    }
+}
---
+++ Ejercicios/Inventario-POO/producto.cs
@@ -0,0 +1,16 @@
+public class producto
+{
+    public string Codigo { get; set; }
+
+    public string Descripcion { get; set; }
+
+    public int Existencia { get; set; }
+
+
+    public Producto( string codigo, string descripcion, int existencia )
+    {
+        Codigo = codigo;
+        Descripcion = descripcion;
+        Existencia = existencia;
+    }
+}

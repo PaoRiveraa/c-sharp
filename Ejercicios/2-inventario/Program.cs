abc53ddf52387cb15cb59f7fa1cf4c1f9a4b9b22
Paola Rivera <priveraa@unah.hn>
2022-07-12 23:40:45 -0600

    agregando hola mundo

---
+++ Ejercicios/2-inventario/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace inventario
+{
+
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WhiteLine("Sistema de Inventario");
+                Console.WhiteLine("*********************");
+                Console.WhiteLine("");
+                Console.WhiteLine("1 - Productos");
+                Console.WhiteLine("2 - Ingreso de Inventario");
+                Console.WhiteLine("3 - Salida de Inventario");
+                Console.WhiteLine("0 - Salir");
+                opcion = Console.Read();
+
+                switch (opcion)
+                {
+                    case 1:
+                    Console.WhiteLine("Productos!");
+                    Console.ReadLine();
+                    break;
+                    default:
+                    break; 
+                    
+                }
+
+                if(opcion == 0){
+                    break;
+                }
+
+
+            }
+            
+        }
+    }
+}
